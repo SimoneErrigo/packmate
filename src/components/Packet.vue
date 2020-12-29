fb3637a8b2d49e57d85d0388fb44a2953009cf85
serega6531 <S.Shkurov@bssys.com>
2020-12-29 14:45:58 +0300

    Безопасное преобразование b64 в юникод

--- src/components/Packet.vue
+++ src/components/Packet.vue
@@ -47,9 +47,16 @@
 			},
 		},
 		methods: {
-      atou(b64) {
-        return decodeURIComponent(escape(atob(b64)));
-      },
+			atou(b64) {
+				const text = atob(b64);
+				const length = text.length;
+				const bytes = new Uint8Array(length);
+				for (let i = 0; i < length; i++) {
+					bytes[i] = text.charCodeAt(i);
+				}
+				const decoder = new TextDecoder();
+				return decoder.decode(bytes);
+			},
 			printPacketFlags(packet) {
 				let flags = [];
 
